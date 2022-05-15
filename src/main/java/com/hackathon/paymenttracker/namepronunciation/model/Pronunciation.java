bc11c1293d0837c0e71b6a59b3bc40de2af897ff
Soumik Pal <pal.soumik33@gmail.com>
2022-05-15 13:01:43 +0530

    Adding Mock Backend

---
+++ src/main/java/com/hackathon/paymenttracker/namepronunciation/model/Pronunciation.java
@@ -0,0 +1,35 @@
+package com.hackathon.paymenttracker.namepronunciation.model;
+
+public class Pronunciation {
+	String name;
+
+	String pronunciation;
+
+	String audio;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPronunciation() {
+		return pronunciation;
+	}
+
+	public void setPronunciation(String pronunciation) {
+		this.pronunciation = pronunciation;
+	}
+
+	public String getAudio() {
+		return audio;
+	}
+
+	public void setAudio(String audio) {
+		this.audio = audio;
+	}
+
+
+}
---
+++ src/main/java/com/hackathon/paymenttracker/namepronunciation/model/PronunciationList.java
@@ -0,0 +1,17 @@
+package com.hackathon.paymenttracker.namepronunciation.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PronunciationList {
+	private List<Pronunciation> pronunciations;
+
+	public List<Pronunciation> getPronunciations() {
+		return pronunciations;
+	}
+
+	public void setPronunciations(List<Pronunciation> pronunciations) {
+		this.pronunciations = pronunciations;
+	}
+
+}
