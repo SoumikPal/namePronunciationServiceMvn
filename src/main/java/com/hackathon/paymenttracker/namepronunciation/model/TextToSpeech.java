37048a3fe8ea72c3b6b437b842f021842c46cd8d
Soumik Pal <pal.soumik33@gmail.com>
2022-05-16 14:58:39 +0530

    Adding Voice and Gender

--- src/main/java/com/hackathon/paymenttracker/namepronunciation/model/TextToSpeech.java
+++ src/main/java/com/hackathon/paymenttracker/namepronunciation/model/TextToSpeech.java
@@ -3,13 +3,27 @@ package com.hackathon.paymenttracker.namepronunciation.model;
 
 public class TextToSpeech {
 	 //String token;
-	 String name;
+	 private String name;
+	 private String gender;
+	 private String voiceregion;
 	 
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getVoiceregion() {
+		return voiceregion;
+	}
+	public void setVoiceregion(String voiceregion) {
+		this.voiceregion = voiceregion;
 	}
 	
 
